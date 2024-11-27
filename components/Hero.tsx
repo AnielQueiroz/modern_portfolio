a3016b53cb333748c815e825b6db8e59ef2cb317
Aniel <anielqsilva@gmail.com>
2024-11-27 17:15:48 -0300

    chore: some settings

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Spotlight } from './ui/spotlight'
 import { TextGenerateEffect } from './ui/text-generate-effect'
-import MagicButton from './ui/MagicButton'
+import MagicButton from './ui/magic-button'
 import { FaLocationArrow } from 'react-icons/fa6'
 
 const Hero = () => {
