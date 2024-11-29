41ccd818622a9013535ae5749c9d6f20d3d2931e
AnielQueiroz <61893813+AnielQueiroz@users.noreply.github.com>
2024-11-29 21:03:44 +0000

    style: rounded yeaah

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -30,7 +30,7 @@ const RecentProjects = () => {
                             href={link}
                         >
                             <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
-                                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
+                                <div className="relative w-full h-full overflow-hidden rounded-lg lg:rounded-3xl bg-[#13162d]">
                                     <img src="/bg.png" alt="bg-img" />
                                 </div>
                                 <img src={img} alt={title} className="z-10 absolute bottom-0" />
