e5972f75aad709f16cb2e4bc48b3937104d883d6
AnielQueiroz <61893813+AnielQueiroz@users.noreply.github.com>
2024-12-02 18:33:56 +0000

    feat: finishing everything again

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
-        <Clients />
+        {/* <Clients /> */}
         <Experience />
         <Approach />
         <Footer />
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,24 @@
-import { FaLocationArrow } from "react-icons/fa6";
+'use client'
+
+import { FaGithub, FaLinkedin, FaLocationArrow } from "react-icons/fa6";
 import MagicButton from "./ui/magic-button";
 import { socialMedia } from "@/data";
+import { FloatingDock } from "./ui/floating-dock";
 
 const Footer = () => {
+	const icons = {
+		Linkedin: <FaLinkedin />,
+		Github: <FaGithub />,
+	}
+
+	const socialWithIcons = socialMedia.map(sm => ({
+		...sm,
+		icon: icons[sm.title]
+	}))
+
 	return (
 		<footer id="contact" className="w-full pb-10 mb-[50px] md:mb-5">
-			<div className="flex flex-col items-center">
+			{/* <div className="flex flex-col items-center">
 				<h1 className="heading lg:max-w-[45vw]">
 					Pronto para trazer <span className="text-purple">suas</span> idéias
 					para a realidade?
@@ -21,7 +34,7 @@ const Footer = () => {
 						position="right"
 					/>
 				</a>
-			</div>
+			</div> */}
 
 			<div className="flex mt-16 md:flex-row flex-col justify-between items-center">
 				<p className="md:text-base text-sm md:font-normal font-light">
@@ -29,14 +42,18 @@ const Footer = () => {
 				</p>
 
 				<div className="flex items-center md:gap-3 gap-6">
-					{socialMedia.map(({ id, img }) => (
+					<FloatingDock 
+						items={socialWithIcons}
+						
+					/>
+					{/* {socialMedia.map(({ id, img }) => (
 						<div
 							key={id}
 							className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
 						>
 							<img src={img} alt={id.toString()} width={20} height={20} />
 						</div>
-					))}
+					))} */}
 				</div>
 			</div>
 		</footer>
