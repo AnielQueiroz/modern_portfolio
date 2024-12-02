1d7ae4893a423c848fe27d439e84b96e40652192
AnielQueiroz <61893813+AnielQueiroz@users.noreply.github.com>
2024-12-02 20:53:54 +0000

    aaaaaaaaaa

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -91,9 +91,12 @@ export const BentoGridItem = ({
 					)}
 				</div>
 				{id === 6 && (
-					<BackgroundGradientAnimation>
-						{/* <div className="absolute z-50 flex items-center justify-center text-white font-bold" /> */}
-					</BackgroundGradientAnimation>
+					<div>
+						aaa
+					</div>
+					// <BackgroundGradientAnimation>
+					// 	<div className="absolute z-50 flex items-center justify-center text-white font-bold" />
+					// </BackgroundGradientAnimation>
 				)}
 
 				<div
