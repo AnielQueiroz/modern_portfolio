00afde160f84577f0b01835c22ca676578b2c824
AnielQueiroz <61893813+AnielQueiroz@users.noreply.github.com>
2024-12-03 18:06:03 +0000

    feat: just a style

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -143,7 +143,7 @@ export const BentoGridItem = ({
 					{id === 6 && (
 						<div className="mt-5 relative">
 							<div className="absolute -bottom-5 right-0">
-								<Lottie
+								{/* <Lottie
 									options={{
 										loop: copied,
 										autoplay: copied,
@@ -152,7 +152,7 @@ export const BentoGridItem = ({
 											preserveAspectRatio: "xMidYMid slice",
 										},
 									}}
-								/>
+								/> */}
 							</div>
 
 							<MagicButton
