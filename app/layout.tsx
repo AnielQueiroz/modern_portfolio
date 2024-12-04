c442ba5b8788f1bdde1ba4f47a2b708f2816fed7
AnielQueiroz <61893813+AnielQueiroz@users.noreply.github.com>
2024-12-04 17:53:50 +0000

    chore: meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "./theme-provider";
-import { useEffect } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,6 +17,28 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Portfólio do Aniel",
   description: "Um moderno e minimalista portfólio com Next.js",
+  openGraph: {
+    title: "Portfólio do Aniel",
+    description: "Explore um portfólio moderno e minimalista construído com Next.js.",
+    url: "https://anieldev.pro",
+    siteName: "Portfólio do Aniel",
+    images: [
+      {
+        url: "https://github.com/AnielQueiroz/modern_portfolio/blob/main/public/banner_port.png?raw=true",
+        width: 1200,
+        height: 630,
+        alt: "Pré-visualização do Portfólio do Aniel",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Portfólio do Aniel",
+    description: "Explore um portfólio moderno e minimalista construído com Next.js.",
+    images: ["https://github.com/AnielQueiroz/modern_portfolio/blob/main/public/banner_port.png?raw=true"],
+  }
 };
 
 export default function RootLayout({
